d5842d0d8a03bbdf2180d2f4eb284112f9f1afa0
luolinlin768 <123939516+luolinlin768@users.noreply.github.com>
2025-05-06 22:56:48 +0800

    修改了目录结构

--- static/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("myBtn").addEventListener("click", () => {
-    alert("按钮已点击！");
-    fetch("/test")  // 示例API调用
-        .then(res => res.json())
-        .then(data => console.log(data));
-});
